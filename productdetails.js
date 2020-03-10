bb6cf49a22dcf103503c432f37c00e976c110a0d
krithi-kondana <krithi1789@gmail.com>
2020-03-10 01:05:25 +0100

    updated cart

--- productdetails.js
+++ productdetails.js
@@ -1,13 +1,122 @@
 let menTshirtArray = [{
         img: 'images/tshirt1.jpeg',
-        name: 'Black t-shirt',
+        name: 'Green full sleves',
         size: 'M size',
         price: '100'
     },
     {
         img: 'images/tshirt2.jpeg',
-        name: 'Grey t-shirt',
+        name: 'Black t-shirt',
         size: 'S size',
         price: '200'
+    },
+    {
+        img: 'images/tshirt3.jpeg',
+        name: 'White t-shirt',
+        size: 'L size',
+        price: '200'
+    },
+    {
+        img: 'images/tshirt4.jpeg',
+        name: 'White t-shirt',
+        size: 'S size',
+        price: '239'
+    },
+    {
+        img: 'images/tshirt5.jpeg',
+        name: 'Rosegold t-shirt',
+        size: 'S size',
+        price: '200'
+    },
+    {
+        img: 'images/tshirt6.jpeg',
+        name: 'Black t-shirt',
+        size: 'M size',
+        price: '199'
+    },
+    {
+        img: 'images/tshirt7.jpeg',
+        name: 'White t-shirt',
+        size: 'S size',
+        price: '299'
+    },
+    {
+        img: 'images/tshirt8.jpeg',
+        name: 'White t-shirt',
+        size: 'XS size',
+        price: '300'
+    },
+    {
+        img: 'images/tshirt9.jpeg',
+        name: 'Blue t-shirt',
+        size: 'S size',
+        price: '250'
+    }, {
+        img: 'images/tshirt10.jpeg',
+        name: 'Skyblue t-shirt',
+        size: 'XL size',
+        price: '200'
+    }
+]
+
+let menJeansArray = [{
+        img: 'images/jeans1.jpeg',
+        name: 'Dark blue jeans',
+        size: 'M size',
+        price: '599'
+    },
+    {
+        img: 'images/jeans2.jpeg',
+        name: 'Sky blue jeans',
+        size: 'M size',
+        price: '450'
+    },
+    {
+        img: 'images/jeans3.jpeg',
+        name: 'Navy blue jeans',
+        size: 'M size',
+        price: '510'
+    },
+    {
+        img: 'images/jeans4.jpeg',
+        name: 'Light blue jeans',
+        size: 'L size',
+        price: '350'
+    },
+    {
+        img: 'images/jeans5.jpeg',
+        name: 'Navy blue jeans',
+        size: 'M size',
+        price: '300'
+    },
+    {
+        img: 'images/jeans6.jpeg',
+        name: 'Dark blue jeans',
+        size: 'S size',
+        price: '400'
+    },
+    {
+        img: 'images/jeans7.jpeg',
+        name: 'Black jeans',
+        size: 'M size',
+        price: '300'
+    },
+    {
+        img: 'images/jeans8.jpeg',
+        name: 'Light blue jeans',
+        size: 'XL size',
+        price: '290'
+    },
+    {
+        img: 'images/jeans9.jpeg',
+        name: 'Dark blue jeans',
+        size: 'L size',
+        price: '399'
+    },
+    {
+        img: 'images/jeans10.jpeg',
+        name: 'Black jeans',
+        size: 'M size',
+        price: '400'
     }
 ]
