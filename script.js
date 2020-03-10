ec5b8b42c432bf064b51736f4cbf1be0ef099f43
krithi-kondana <krithi1789@gmail.com>
2020-03-10 01:04:37 +0100

    updated cart

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-let products = document.querySelector('.container-fluid .d-flex')
+let container = document.querySelector('.container-fluid .d-flex')
 
 
-function renderProducts() {
-    products.textContent = '';
-    menTshirtArray.forEach(tshirt => {
+
+function renderProducts(clickedId) {
+    let products = [];
+    container.textContent = '';
+    if (clickedId === 'tshirts') {
+        products.push(...menTshirtArray)
+    } else if (clickedId === 'jeans') {
+        products.push(...menJeansArray)
+    }
+    products.forEach(item => {
         let div = document.createElement('div')
         let img = document.createElement('img')
         let details = document.createElement('p')
         let button = document.createElement('button');
 
-        details.innerHTML = `<strong>${tshirt.name}<strong> <br> ${tshirt.size} <br> ${tshirt.price}dkk`
-        img.src = tshirt.img;
+        details.innerHTML = `<strong>${item.name}<strong> <br> ${item.size} <br> ${item.price}dkk`
+        img.src = item.img;
         button.innerText = 'Add to cart';
 
-        products.appendChild(div)
+        container.appendChild(div)
         div.append(img, details, button)
 
         button.addEventListener("click", () => {
-            addItemsToCart(tshirt)
+            addItemsToCart(item)
         })
 
     })
@@ -25,22 +32,29 @@ function renderProducts() {
 
 let cartUl = document.querySelector('.cart ul');
 let total = document.querySelector('.total')
+let h4 = document.querySelector('.cart h4')
 let priceArray = [];
 let newArray = []
 
-function addItemsToCart(tshirt) {
-
+function addItemsToCart(item) {
     total.innerHTML = '';
-
+    // section.innerHTML = `<h4>Your cart</h4>`
     let li = document.createElement('li');
     let p = document.createElement('p')
+    h4.innerText = 'Your cart';
     let deleteButton = document.createElement('button')
     deleteButton.innerText = 'Delete';
+    // let input = document.createElement('input')
+    // input.type = 'number';
+    // input.value = '';
+    // input.id = 'quantity';
+    // let quantity = input.value;
 
-    li.innerHTML = `<div>${tshirt.name}</div>
-    <div> ${tshirt.price}dkk</div>`
 
-    priceArray.push(tshirt.price);
+    li.innerHTML = `<div>${item.name}</div>
+    <div> ${item.price}dkk</div>`
+
+    priceArray.push(item.price);
 
     let priceAsNumber = priceArray.map(Number)
 
@@ -53,7 +67,7 @@ function addItemsToCart(tshirt) {
     deleteButton.addEventListener('click', () => {
         cartUl.removeChild(li);
 
-        let deletedProductPrice = Number(tshirt.price)
+        let deletedProductPrice = Number(item.price)
         newArray.push(deletedProductPrice);
         let lastItem = newArray[newArray.length - 1]
 
@@ -67,11 +81,15 @@ function addItemsToCart(tshirt) {
         console.log('deletedprice', deletedProductPrice);
 
     })
-
     cartUl.appendChild(li);
-    li.appendChild(deleteButton)
+    li.append(deleteButton)
     total.appendChild(p)
 }
+
+// let bag = document.querySelector('.bag')
+// bag.addEventListener('click', showCart)
+
+
 
 
 // function deleteItem(element) {
